4238a3a32923cadc8d3a89b35effad1d2ade4d54
MakerSquare Student <students@themakersquare.com>
2013-12-05 14:39:35 -0600

    add code to define an average rating

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,16 @@ class Video < ActiveRecord::Base
   belongs_to :user
   validates :user_id, :title, :youtube_id, presence: true
   has_many :ratings
+
+  def avg_rating 
+  	total = 0.0
+  	self.ratings.to_a.each do |rating|
+  		total += rating.value
+  	end
+  	if self.ratings.count == 0
+  		return 0
+  	else
+  		return total / self.ratings.count
+  	end
+  end
 end
