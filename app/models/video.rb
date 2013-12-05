d1939cf4c45a273883a37897f1dd6320f30a02df
MakerSquare Student <students@themakersquare.com>
2013-12-05 14:54:33 -0600

    Refacctor average rating method to one line

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,14 +4,6 @@ class Video < ActiveRecord::Base
   has_many :ratings
 
   def avg_rating 
-  	total = 0.0
-  	self.ratings.to_a.each do |rating|
-  		total += rating.value
-  	end
-  	if self.ratings.count == 0
-  		return 0
-  	else
-  		return total / self.ratings.count
-  	end
+  	self.ratings.average(:value).to_i
   end
 end
